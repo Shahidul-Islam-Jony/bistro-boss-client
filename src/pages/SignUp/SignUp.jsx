c46965729542acf6e830ac7894a4181ee5c5161a
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-13 16:50:00 +0600

    M(66-5) Explore React hook form validation error handling

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,18 @@
-
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,24 +21,27 @@ const SignUp = () => {
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="Your name" name='name' className="input input-bordered" required />
+                            <input type="text" {...register("name",{ required: true })} placeholder="Your name" name='name' className="input input-bordered" />
+                            {errors.name && <span className="text-red-600">Name is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" name='email' className="input input-bordered" required />
+                            <input type="email" {...register("email",{ required: true })} placeholder="email" name='email' className="input input-bordered" />
+                            {errors.email && <span className="text-red-600">Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" name='password' className="input input-bordered" required />
+                            <input type="password" {...register("password",{ required: true, minLength:6, maxLength: 20 })} placeholder="password" name='password' className="input input-bordered"  />
+                            {errors.password && <span className="text-red-600">Password is required</span>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
