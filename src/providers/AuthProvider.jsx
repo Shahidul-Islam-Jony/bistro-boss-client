a925c5b8369dba670fb64b806ae4bb3ae2db4819
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-13 11:44:27 +0600

    M(66-3) FireBase Project Setup

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,26 @@
+import { getAuth } from "firebase/auth";
+import { createContext, useState } from "react";
+import { app } from "../firebase/firebase.config";
+
+
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+    const [user,setUser] = useState(null);
+    const [loading,setLoading] = useState(true);
+
+
+    const authInfo = {
+        user,
+        loading,
+
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
