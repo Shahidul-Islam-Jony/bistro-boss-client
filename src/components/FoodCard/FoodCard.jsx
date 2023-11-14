52db8ef12351252f6a89e75a9635d4653e19df02
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-14 23:31:49 +0600

    M(67-5) (Interesting) Show add to cart count using refetch

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,6 +2,7 @@ import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 
 
@@ -11,11 +12,11 @@ const FoodCard = ({ item }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const axiosSecure = useAxiosSecure();
+  const [,refetch] = useCart();   //prothom e cart ace seti ignore korte , 
 
-  const handleAddToCart = food => {
-    console.log(food);
+  const handleAddToCart = () => {
     if (user && user.email) {
-      // TODO: send cart item to the database
+      // send cart item to the database
       const cartItem = {
         menuId: _id,
         email: user.email,
@@ -35,6 +36,8 @@ const FoodCard = ({ item }) => {
               showConfirmButton: false,
               timer: 2000
             });
+            // refetch cart to update the cart items count
+            refetch()
           }
         })
 
@@ -65,7 +68,7 @@ const FoodCard = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button onClick={() => handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+          <button onClick={handleAddToCart} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
         </div>
       </div>
     </div>
