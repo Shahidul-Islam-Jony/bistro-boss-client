b2b2effebb5a9b568cb51e4e3e457b2b46f32848
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-14 21:05:44 +0600

    M(67-2) Enforce user to  login before add to the cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,25 +1,51 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 
-const FoodCard = ({item}) => {
-    const {image,price,name,recipe} = item;
+const FoodCard = ({ item }) => {
+  const { image, price, name, recipe } = item;
+  const { user } = useAuth();   //using custom hooks
+  const navigate = useNavigate();
 
-    const handleAddToCart = food =>{
-      console.log(food);
+
+  const handleAddToCart = food => {
+    // console.log(food);
+    if (user && user.email) {
+      // TODO: send cart item to the database
+
+    }
+    else {
+      Swal.fire({
+        title: "You are not Logged In",
+        text: "Please login to add to the cart?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Login!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/login');
+        }
+      });
     }
 
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-        <figure><img src={image} alt="Shoes" /></figure>
-        <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
-        <div className="card-body flex flex-col items-center">
-          <h2 className="card-title">{name}</h2>
-          <p>{recipe}</p>
-          <div className="card-actions justify-end">
-            <button onClick={()=>handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
-          </div>
+  }
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure><img src={image} alt="Shoes" /></figure>
+      <p className="absolute right-0 mr-4 mt-4 px-4 bg-slate-900 text-white">${price}</p>
+      <div className="card-body flex flex-col items-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-end">
+          <button onClick={() => handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default FoodCard;
