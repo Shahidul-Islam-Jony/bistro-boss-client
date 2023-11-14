78587fa9de900e5f2f4f8325728f0bb99cd993ea
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-15 00:31:31 +0600

    M(67-7) Cart data from custom hook and calculate total price

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaBars, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 
@@ -11,30 +11,29 @@ const Dashboard = () => {
                     <li>
                         <NavLink to='/dashboard/userHome'><FaHome></FaHome>User Home</NavLink>
                     </li>
-                </ul>
-                <ul className="menu p-4">
                     <li>
                         <NavLink to='/dashboard/reservation'><FaCalendar></FaCalendar>Reservation</NavLink>
                     </li>
-                </ul>
-                <ul className="menu p-4">
                     <li>
                         <NavLink to='/dashboard/cart'><FaShoppingCart></FaShoppingCart> My Cart</NavLink>
                     </li>
-                </ul>
-                <ul className="menu p-4">
                     <li>
                         <NavLink to='/dashboard/review'><FaAd></FaAd>Add a Review</NavLink>
                     </li>
-                </ul>
-                <ul className="menu p-4">
                     <li>
                         <NavLink to='/dashboard/bookings'><FaList></FaList>My Bookings</NavLink>
+                    </li>
+                    <div className="divider"></div>
+                    <li>
+                        <NavLink to='/'><FaHome></FaHome>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/menu'><FaBars></FaBars>Menu</NavLink>
                     </li>
                 </ul>
             </div>
             {/* dashboard content */}
-            <div className="flex-1">    {/*flex-grow othoba flex-1 dile baki with tuku full nibe*/}
+            <div className="flex-1 p-8">    {/*flex-grow othoba flex-1 dile baki with tuku full nibe*/}
                 <Outlet></Outlet>
             </div>
         </div>
