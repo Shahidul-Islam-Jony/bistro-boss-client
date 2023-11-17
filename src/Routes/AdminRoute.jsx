ea9b79434e5fef27282daa711d2c1608d9305b99
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-17 12:03:58 +0600

    M(68-10) (advanced) Make user api secure using verify Admin

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -1,22 +1,25 @@
-
 import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const PrivateRoute = ({ children }) => {
+
+const AdminRoute = (children) => {
     const { user, loading } = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+
     const location = useLocation();
 
-    if (loading) {
+    if (loading || isAdminLoading) {
         return <div>
             <span className="loading loading-ring loading-md"></span>
             <span className="loading loading-spinner loading-lg"></span>
         </div>
     }
-    if (user) {
+    if (user && isAdmin) {
         return children;
     }
 
-    return <Navigate state={{from:location}} to='/login'></Navigate>
+    return <Navigate state={{ from: location }} to='/login'></Navigate>
 };
 
-export default PrivateRoute;
+export default AdminRoute;
--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -4,17 +4,17 @@ import useAxiosSecure from "./useAxiosSecure";
 
 
 const useAdmin = () => {
-    const {user} = useAuth();
+    const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    const {data:isAdmin}=useQuery({
-        queryKey:[user?.email,'isAdmin'],
-        queryFn: async()=>{
+    const { data: isAdmin, isPending: isAdminLoading } = useQuery({
+        queryKey: [user?.email, 'isAdmin'],
+        queryFn: async () => {
             const res = await axiosSecure.get(`/users/admin/${user.email}`)
             console.log(res.data);
             return res.data?.admin;
         }
     })
-    return [isAdmin];
+    return [isAdmin,isAdminLoading];
 };
 
 export default useAdmin;
