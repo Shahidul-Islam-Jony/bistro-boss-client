40ab11242e91fa64adbe566ce5b16339349fc079
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-11-19 00:45:41 +0600

    M(69-5)  Save menu item to the server and make api secure

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -2,24 +2,51 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit,reset } = useForm();
     const axiosPublic = useAxiosPublic();
-    const onSubmit = async(data) => {
+    const axiosSecure = useAxiosSecure();
+    const onSubmit = async (data) => {
         // console.log(data);
         // image upload to imgbb and then get an url
-        const imageFile = {image: data.image[0]}
-        const res = await axiosPublic.post(image_hosting_api, imageFile,{
-            headers:{
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
                 'Content-Type': 'multipart/form-data'   //search google how to post image to imgbb using axios react and see stackoverflow website 
             }
         })
-        console.log(res.data);
+        console.log('With image url', res.data);
+        if (res.data.success) {
+            // now send the menu item data to the server with the image url
+            const menuItem = {
+                name: data.name,
+                category: data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image: res.data.data.display_url
+            }
+            // now send data to backend
+            const menuRes = await axiosSecure.post('/menu', menuItem);
+            console.log(menuRes.data);
+            if (menuRes.data.insertedId) {
+                //show success popup
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.name} is added to the menu`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        }
     }
     return (
         <div>
@@ -30,7 +57,7 @@ const AddItems = () => {
                         <label className="label">
                             <span className="label-text">Recipe Name*</span>
                         </label>
-                        <input {...register('name')} type="text" placeholder="Recipe name" className="input input-bordered w-full"  required/>
+                        <input {...register('name')} type="text" placeholder="Recipe name" className="input input-bordered w-full" required />
                     </div>
 
                     <div className="flex gap-6">
@@ -39,7 +66,7 @@ const AddItems = () => {
                             <label className="label">
                                 <span className="label-text">Category*</span>
                             </label>
-                            <select defaultValue="default" {...register('category',{required:true})} className="select select-bordered w-full">
+                            <select defaultValue="default" {...register('category', { required: true })} className="select select-bordered w-full">
                                 <option disabled value='default'>Select a category</option>
                                 <option value="salad">Salad</option>
                                 <option value="pizza">Pizza</option>
@@ -54,7 +81,7 @@ const AddItems = () => {
                             <label className="label">
                                 <span className="label-text">Price*</span>
                             </label>
-                            <input {...register('price',{required:true})} type="number" placeholder="Price" className="input input-bordered w-full" />
+                            <input {...register('price', { required: true })} type="number" placeholder="Price" className="input input-bordered w-full" />
                         </div>
                     </div>
                     {/* Recipe details */}
@@ -66,7 +93,7 @@ const AddItems = () => {
                     </div>
 
                     <div className="form-control w-full my-6">
-                        <input {...register('image',{required:true})} type="file" className="file-input w-full max-w-xs" />
+                        <input {...register('image', { required: true })} type="file" className="file-input w-full max-w-xs" />
                     </div>
 
                     <button className="btn">Add Item <FaUtensils className="ml-2"></FaUtensils></button>
